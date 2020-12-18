7370e98aea9a158074071aa94042a2aa31be9f73
brian <brianadamson87@gmail.com>
2020-12-18 07:17:11 -0700

    Login attempt text log working.

--- src/View_Controller/LoginController.java
+++ src/View_Controller/LoginController.java
@@ -6,12 +6,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -31,27 +35,29 @@ public class LoginController  implements Initializable {
         if (UserName.getText().equals("") || Password.getText().equals("")){
             MessageModal.display("Unable to login", "Please enter user name and password");
         } else {
-            String un = UserName.getText();
-            String pw = Password.getText();
-            String query = "Select * from users where User_Name = '" + un + "' and password = '" + pw + "'";
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            if (rs.next())
-            {
-                int id = rs.getInt("User_ID");
-                String userName = rs.getString("User_Name");
-                String password = rs.getString("Password");
-                Date createDate = rs.getDate("Create_Date");
-                String createBy = rs.getString("Created_By");
-                Date lastUpdate = rs.getDate("Last_Update");
-                String updateBy = rs.getString("Last_Updated_By");
-                User newUser = new User(id, userName, password, createDate, createBy, lastUpdate, updateBy);
-                this.setUser(newUser);
-            } else
-                {
+            try {
+                String un = UserName.getText();
+                String pw = Password.getText();
+                String query = "Select * from users where User_Name = '" + un + "' and password = '" + pw + "'";
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                if (rs.next()) {
+                    int id = rs.getInt("User_ID");
+                    String userName = rs.getString("User_Name");
+                    String password = rs.getString("Password");
+                    Date createDate = rs.getDate("Create_Date");
+                    String createBy = rs.getString("Created_By");
+                    Date lastUpdate = rs.getDate("Last_Update");
+                    String updateBy = rs.getString("Last_Updated_By");
+                    User newUser = new User(id, userName, password, createDate, createBy, lastUpdate, updateBy);
+                    this.setUser(newUser);
+                } else {
                     MessageModal.display("Unable to Login", "Authorization failed");
-                    // todo record failed login attempt?
+                    this.recordLoginAttempt(un, false);
                 }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
         }
     }
 
@@ -63,7 +69,25 @@ public class LoginController  implements Initializable {
     private void setUser(User user) throws IOException {
             HomeController.loggedInUser = user;
             this.navigateHome();
-            // todo record logged in user file
+            try{
+                this.recordLoginAttempt(user.getUser_Name(), true);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+    }
+
+    /**
+     * record user log in attempts and locations
+     * @param user user name of attempted login
+     * @param successful if login was successful
+     */
+    private void recordLoginAttempt(String user, boolean successful) throws IOException {
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime ldc = LocalDateTime.now();
+        FileWriter fw = new FileWriter("loginRecord.txt", true);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println ("Name: " + user + " Location: " + zoneId + " DateTime: " + ldc + " Successful Log in: " + successful);
+        pw.close();
     }
 
     /**
